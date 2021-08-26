d1960332f9b1bd62234f3bd6308341a34ca455d6
Jedidiah Schloesser <jedschloesser@gmail.com>
2021-08-26 14:52:52 -0500

    updated header

--- root/frontend/src/components/Header.js
+++ root/frontend/src/components/Header.js
@@ -1,14 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import Paper from '@material-ui/core/Paper';
+import HomeIcon from '@material-ui/icons/Home';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    icon: {
+      color: "#f7f7f7",
+      fontSize: "200%",
+    }
+  });
 
 export function Header() {
+    const classes = useStyles();
+
     return (
         <Paper elevation={5}>
             <div>
                 <nav className="navbar">
+                    <a className="ms-3" href="/">
+                        <HomeIcon className={classes.icon}/>
+                    </a>
                     <a className="navbar-brand mx-auto" href="/">
                         <span>UW-Madison Course Search</span>
+                    </a>
+                    <a className="me-3" href="/">
+                        <FavoriteIcon className={classes.icon} />
                     </a>
                 </nav>
             </div>
